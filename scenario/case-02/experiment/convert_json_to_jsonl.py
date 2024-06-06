ecf3852ea6915b76ee9fe7dadb590764f242f6a2
Nghiem Thanh Pham <nghiempt@Nghiems-MacBook-Pro.local>
2024-06-07 00:19:22 +0700

    init: draft version

---
+++ scenario/case-02/experiment/convert_json_to_jsonl.py
@@ -0,0 +1,17 @@
+import json
+
+# Input JSON file path
+input_json_file = '/Users/nghiempt/Src/Research/conf_a&p_2025/scenario/case-02/data/150-train-actual.json'
+
+# Output JSONL file path
+output_jsonl_file = '/Users/nghiempt/Src/Research/conf_a&p_2025/scenario/case-02/data/150-train-actual.jsonl'
+
+# Open the input JSON file for reading and the output JSONL file for writing
+with open(input_json_file, 'r') as json_file, open(output_jsonl_file, 'w') as jsonl_file:
+    # Load the entire JSON file as a list of JSON objects
+    data = json.load(json_file)
+    
+    # Iterate through the list of JSON objects
+    for item in data:
+        # Serialize each object as a JSON string and write it to the JSONL file followed by a newline character
+        jsonl_file.write(json.dumps(item) + '\n')
---
+++ scenario/case-08/experiment/process-outcome.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+# Read the CSV file
+df = pd.read_csv('/Users/nghiempt/Src/Research/conf_a&p_2025/scenario/case-08/data/150-test-ft.csv')
+
+# Function to extract the values from the result column
+def extract_result_values(result):
+    result_dict = json.loads(result.replace("'", "\""))
+    return pd.Series([result_dict.get('incorrect', 0), result_dict.get('incomplete', 0), result_dict.get('inconsistent', 0)])
+
+# Apply the function to the result column
+df[['incorrect', 'incomplete', 'inconsistent']] = df['result'].apply(extract_result_values)
+
+# Drop the original result column
+df = df.drop(columns=['result'])
+
+# Export the modified DataFrame to a new CSV file
+df.to_csv('/Users/nghiempt/Src/Research/conf_a&p_2025/scenario/case-08/data/150-test-ft-process.csv', index=False)
+
+print("CSV file has been successfully created.")
